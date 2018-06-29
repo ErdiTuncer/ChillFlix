2079208b94b244d0cf05434c81b1a62df846c6d9
Claudia Hoogwerf <claudia.ps.hoogwerf@kpn.com>
2018-06-29 16:25:48 +0200

    Fix for template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<youtube-player
-      [videoId]="id"
-      (ready)="savePlayer($event)"
-      (change)="onStateChange($event)"
-    ></youtube-player>`
 })
 export class AppComponent {
   title = 'app';
